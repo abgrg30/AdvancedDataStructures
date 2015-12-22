39ce7df9951c15769176e5a342049768dce5bd0e
Abhinav Garg <abgarg@ucsd.edu>
2015-12-21 20:33:34 -0800

    Print root to leaf paths

--- Trees/BTRoot2Leaf.cpp
+++ Trees/BTRoot2Leaf.cpp
@@ -1,5 +1,4 @@
 #include<iostream>
-#include<queue>
 using namespace std;
 
 struct node
@@ -8,7 +7,7 @@ struct node
     node* left;
     node* right;
 
-    node(int d)
+    node(int d): data(0)
     {
         data = d;
         left = NULL;
@@ -23,61 +22,103 @@ struct node
     }
 };
 
-void del(node* ptr)
+node* gettreenode(int data)
 {
-    if(ptr)
+    try
     {
-        del(ptr->left);
-        del(ptr->right);
-        delete ptr;
+        node* ptr = new node(data);
+        cout<<"Node "<<data<<" Created!!\n";
+        return ptr;
+    }
+    catch(...)
+    {
+        cout<<"EXCEPTION : Node Creation Failed!!\n";
+        return NULL;
     }
 }
 
-vector<node*> q;
-
-void printqueue()
+void deltree(node* root)
 {
-    vector<node*>::iterator i;
-
-    for(i=q.begin();i!=q.end();i++)
+    if(root)
     {
-        cout<<(*i)->data;
+        deltree(root->left);
+        deltree(root->right);
+        cout<<"\nNode "<<root->data<<" deleted";
+        delete root;
     }
+    else
+    {
+        return;
+    }
+}
 
+int treesize(node* root)
+{
+    if(root == NULL)
+    {
+        return 0;
+    }
+    else
+    {
+        return treesize(root->left) + 1 + treesize(root->right);
+    }
+}
+
+void printarr(int arr[], int start, int finish)
+{
+    for(int i=start;i<=finish;i++)
+    {
+        cout<<arr[i]<<"\t";
+    }
     cout<<endl;
 }
 
-void roottoleaf(node* ptr)
+void root2leaf(node* root, int arr[], int pos)
 {
-    if(ptr)
+    if(root == NULL)
     {
-        q.push_back(ptr);
-
-        if(!ptr->left && !ptr->right)
+        return;
+    }
+    else
+    {
+        if(root->left == NULL && root->right == NULL)
         {
-            printqueue();
-            q.pop_back();
+            arr[pos] = root->data;
+            printarr(arr,0,pos);
         }
         else
         {
-            roottoleaf(ptr->left);
-            roottoleaf(ptr->right);
-            q.pop_back();
+            arr[pos] = root->data;
+            root2leaf(root->left,arr,pos+1);
+            root2leaf(root->right,arr,pos+1);
         }
     }
 }
 
 int main()
 {
-    node* root = new node(10);
-    root->left = new node(8);
-    root->right = new node(2);
-    root->right->left = new node(2);
-    root->left->right = new node(5);
-    root->left->left = new node(3);
+    node* root = gettreenode(20);
+    root->left = gettreenode(8);
+    root->right = gettreenode(22);
+    root->right->right = gettreenode(25);
+    root->left->left = gettreenode(5);
+    root->left->right = gettreenode(3);
+    root->left->right->left = gettreenode(10);
+    root->left->right->right = gettreenode(14);
 
-    roottoleaf(root);
+    int n = treesize(root);
+    //cout<<n<<endl;
 
-    del(root);
+    int arr[n];
+
+    for(int i=0; i<n; i++)
+    {
+        arr[i] = 0;
+    }
+
+    root2leaf(root,arr,0);
+
+    deltree(root);
+
     return 0;
 }
